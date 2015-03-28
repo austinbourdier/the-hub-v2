fe2033dd40e760571cbf220acc0b6672c8f910a7
Austin Bourdier <austinbourdier@gmail.com>
2015-03-27 22:11:17 -0700

    update node version type

--- controller/db.js
+++ controller/db.js
@@ -10,16 +10,17 @@ exports.getUserTextsAndEmails = function(req,res,next){
       request({url:(process.env.text_service_url || require("../config.js").get('text_service').url) + '/1/texts',
       method: "GET",
       "content-type": 'application/json',
-      headers:{"x-apikey":process.env.textApiKey || require('../config.js').get('text_service:apikey')}, "x-user-id": req.session.user._id, "content-type":'application/json'}, function (error, response, body) {
+      headers:{"x-apikey":process.env.textApiKey || require('../config.js').get('text_service:apikey'), "x-user-id": req.session.user._id, "content-type":'application/json'}}, function (error, response, body) {
         if (error) return done('TEXT SERVICE ERROR: ' + error);
         done(null, {texts: response.body});
       })
     },
     function(data, done){
       console.log('yolo 1.5')
-      request({url:(process.env.email_service_url || require("../config.js").get('email_service').url) + '/1/emails', "content-type": 'application/json',
+      request({url:(process.env.email_service_url || require("../config.js").get('email_service').url) + '/1/emails',
+      "content-type": 'application/json',
       method: "GET",
-      headers:{"x-apikey":process.env.emailApiKey || require('../config.js').get('email_service:apikey')}, "x-user-id": req.session.user._id, "content-type":'application/json'}, function (error, response, body) {
+      headers:{"x-apikey":process.env.emailApiKey || require('../config.js').get('email_service:apikey'), "x-user-id": req.session.user._id, "content-type":'application/json'}}, function (error, response, body) {
         if (error) return done('EMAIL SERVICE ERROR: ' + error);
         data.emails =  response.body;
         done(null, data);
