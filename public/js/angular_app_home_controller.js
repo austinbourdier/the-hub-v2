4a9c4358df11a037f29ec247a4ae3cfc7d418c9f
Scott Jason <scottleejason@gmail.com>
2015-05-10 06:01:50 -0700

    broadcasts user obj to controller from directive and logs to console, updates package.json to safe version of mongoose

--- public/js/angular_app_home_controller.js
+++ public/js/angular_app_home_controller.js
@@ -3,47 +3,55 @@ angular.module('mainApp')
   .service('UserService', UserService)
   .directive('dropzone', DropzoneDirective);
 
-function homeController($scope, UserService) {
-  this.user = user;
-  this.dropboxLogin = function(){
+function homeController($rootScope, $scope, UserService) {
+  $scope.user = user;
+  $scope.$watch('user', function() {
+    if ($scope.user.email) {
+    $rootScope.$broadcast('userLoggedIn', $scope.user);
+    }
+  })
+
+  this.dropboxLogin = function() {
     UserService.logIn('dropbox');
   }
-  this.boxLogin = function(){
+  this.boxLogin = function() {
     UserService.logIn('box');
   }
-  this.googleDriveLogin = function(){
+  this.googleDriveLogin = function() {
     UserService.logIn('googledrive');
   }
 }
 
-function UserService($window){
-  this.logIn = function(cloud){
+function UserService($window) {
+  this.logIn = function(cloud) {
     $window.location = $window.location.protocol + '//' + $window.location.host + '/auth/' + cloud + '/login';
   };
 }
 
+
 function DropzoneDirective() {
-  return function (scope, element, attrs) {
+  return function(scope, element, attrs) {
     var dropzoneConfig = {
-      'options': { // passed into the Dropzone constructor
-        'url': '/file-upload'
-      },
-      'eventHandlers': {
-        'error': function (file, xhr, formData) {
-          // TODO: error catch
+        'options': { // passed into the Dropzone constructor
+          'url': '/file-upload'
         },
-        'success': function (file, response, body) {
-          // TODO: error catch
-          this.user = response.user;
-          this.$apply()
+        'eventHandlers': {
+          'error': function(file, xhr, formData) {
+            // TODO: error catch
+          },
+          'success': function(file, response, body) {
+            console.log(file, response, body)
+              // TODO: error catch
+            scope.user = response.user;
+            // this.$apply()
+          }
         }
       }
-    }
-    // create a Dropzone for the element with the given options
+      // create a Dropzone for the element with the given options
     var dropzone = new Dropzone(element[0], dropzoneConfig.options);
 
     // bind the given event handlers
-    angular.forEach(dropzoneConfig.eventHandlers, function (handler, event) {
+    angular.forEach(dropzoneConfig.eventHandlers, function(handler, event) {
       dropzone.on(event, handler);
     });
   };
