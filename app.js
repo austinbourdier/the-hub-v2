a2861de8c0f9fcb138c8f82d1db3b252533b22b7
Austin Bourdier <austinbourdier@compology.us>
2015-12-24 23:07:47 -0800

    deploying for CORS testing

--- app.js
+++ app.js
@@ -45,7 +45,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sessionOpts));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://limitless-earth-7143.herokuapp.com/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 routes(app, passport);
 
 
