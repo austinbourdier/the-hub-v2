cc9d9fa22e1ecf1b6bf25b8e99102e1feaa9bc19
Austin Bourdier <abourdier@demandbase.com>
2016-01-21 16:52:36 -0800

    $state.reload() fixed that accordion issue

--- public/js/directives/cloudFiles.js
+++ public/js/directives/cloudFiles.js
@@ -1,7 +1,7 @@
 app
 .directive('cloudFiles', cloudFiles)
 
-function cloudFiles($compile) {
+function cloudFiles($compile, $state) {
   return {
     restrict: 'AE',
     templateUrl: "../../views/directives/cloud_files.html",
@@ -13,7 +13,6 @@ function cloudFiles($compile) {
     },
     controller: function($scope, toastr, FileService, UserService) {
       $scope.newTitle = {};
-      $scope.oldTitle = {};
       $scope.deleteFromCloud = function(id, cloud) {
         FileService.delete(id, cloud).then(function(data) {
           toastr.success('Your File Was Deleted From ' + cloud.charAt(0).toUpperCase() + cloud.slice(1));
@@ -30,20 +29,15 @@ function cloudFiles($compile) {
         FileService.renameFile(id, $scope.title.replace(" ", ""), $scope.newTitle[id]).then(function(data) {
           toastr.success("Your File's Name Was Updated!");
           $scope.user = UserService.normalizeUser(data.user);
-          var elementStr = "<h4 class='panel-title'><a href='' tabindex='0' class='accordion-toggle' ng-click='toggleOpen()' uib-accordion-transclude='heading'><span ng-class='{'text-muted': isDisabled}' class='ng-binding'>" + $scope.newTitle[id] + "</span></a></h4>";
-          $compile(elementStr)($scope)
-          angular.element('#update-name').replaceWith(elementStr);
-        }, function(err) {
+          $state.reload();
           if(err == 'Not Authorized')
             toastr.error("You are not authorized to update this file! It's probably shared or owner by other users.");
           else
             toastr.error("Something went wrong!");
-          angular.element('#update-name').replaceWith('<span>' + $scope.oldTitle[id] + '</span>');
+          $state.reload();
         });
       }
       $scope.changeToInputField = function($event, id, title) {
-        console.log(title)
-        $scope.oldTitle[id] = title;
         $scope.newTitle[id] = title;
         var elementStr = '<form id="update-name" ng-submit="renameFile(' + "'" + id + "'" + ')"><input ng-model="newTitle[' + "'" + id + "'" + ']" value=' + title.replace(" ", "&nbsp;") + '></input></form>';
         angular.element(angular.element($event.target).parents()[3].children[0].children[0]).replaceWith($compile(elementStr)($scope));
